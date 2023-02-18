68d16292824a3c2ccd016ebb26756075e46c4505
HyperCaption <alex.faehndrich@gmail.com>
2023-02-18 16:42:33 +0100

    changes

--- modules/pullcover.py
+++ modules/pullcover.py
@@ -1,16 +1,16 @@
 import urllib.request
-
+import mutagen
 import re
 
 class SpotifyCoverLoader():
     def __init__(self) -> None:
         pass
 
-    def get_thumb_url(self):
+    def get_thumb_url(self,album_url):
         pattern = r'loading="eager" src="([^ ]*)"'
 
         #do not know why, but it works; finds thumbnail url using regex
-        req = urllib.request.urlopen('https://open.spotify.com/track/1qsexgIQui6hZ8tAGoEg4G').read().decode('utf-8').encode('cp850','replace').decode('cp850')
+        req = urllib.request.urlopen(album_url).read().decode('utf-8').encode('cp850','replace').decode('cp850')
         thumbnail_url = re.findall(pattern,req)
 
         print(thumbnail_url)
@@ -18,6 +18,8 @@ class SpotifyCoverLoader():
 
     def download_cover(self):
         print(self.thumbnail_url)
-        urllib.request.urlretrieve(self.thumbnail_url,"./out/local-filename.jpg")
+        urllib.request.urlretrieve(self.thumbnail_url, f"./out/albumcover.jpg")
 
+    def merge_cover_mp3(self):
+        pass
         
