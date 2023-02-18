f4e48b71b460dc9d59f52ba7dca73aaf707d13d5
HyperCaption <alex.faehndrich@gmail.com>
2023-02-18 14:32:27 +0100

    changed playlist

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from modules.yt_api import YoutubeApi
 
 def main() :
     sp_api = SpotifyApi()
-    sp_api.get_songs_from_playlist("OfficerAlex","0Fm8D19xbDnZlVXro7UzLC")
+    sp_api.get_songs_from_playlist("OfficerAlex","0jZnctaVSmdfqtwdGGmhDO")
     song_object_list = sp_api.format_output()
 
     yt = YoutubeApi()
