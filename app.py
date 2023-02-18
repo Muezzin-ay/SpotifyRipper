ecbc5ad0e0e7761c4ace48984d60deb2b04563cd
Robin Hart <hartrobin05@gmail.com>
2023-02-18 16:31:47 +0100

    removed login data

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 
+import json
+import os
+
 from modules.spotify import SpotifyApi
 from modules.yt_api import YoutubeApi
-#from modules.song import Song
+
+
+def check_settings_file() :
+    if not os.path.isfile('./config.json') :
+        with open('./config.json', 'w') as file:
+            file.write('{"api_client" : "", "api_secret" : "", "spotify_username" : ""}')
+            return False
+    return True
+
+def load_settings() :
+    with open('./config.json') as file:
+        settings = json.load(file)
+        file.close()
+    return settings
 
 
 def main() :
-    sp_api = SpotifyApi()
-    sp_api.get_songs_from_playlist("OfficerAlex","0jZnctaVSmdfqtwdGGmhDO")
+    settings = load_settings()
+    sp_api = SpotifyApi(settings['api_client'], settings['api_secret'])
+
+    album_creator = settings['spotify_username']
+    album_id = "0jZnctaVSmdfqtwdGGmhDO"
+    sp_api.get_songs_from_playlist(album_creator, album_id)
+
     song_object_list = sp_api.format_output()
 
     yt = YoutubeApi()
     scratched_urls = []
 
-    for song in song_object_list[:4] :
+    for song in song_object_list[:1] :
         url = yt.search_song(song)
         scratched_urls.append(url)
 
@@ -21,4 +42,6 @@ def main() :
 
 
 if __name__ == '__main__' :
+    if check_settings_file() :
+        False
     main()
