3c4192ee77328834a90cf200b7267a7dc32f9ef4
HyperCaption <alex.faehndrich@gmail.com>
2023-02-20 20:05:01 +0100

    integrated multiprocessing option

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from modules.yt_api import YoutubeApi
 from modules.pullcover import SpotifyCoverLoader
 from modules.str_tools import gen_file_name
 
+from modules.multiprocess import MultiprocessStart
+
 def check_settings_file() :
     if not os.path.isfile('./config.json') :
         with open('./config.json', 'w') as file:
@@ -20,8 +22,7 @@ def load_settings() :
         file.close()
     return settings
 
-
-def main() :
+def pull_spotify_info():
     settings = load_settings()
     sp_api = SpotifyApi(settings['api_client'], settings['api_secret'])
 
@@ -31,18 +32,42 @@ def main() :
 
     song_object_list = sp_api.format_output()
 
-    yt = YoutubeApi()
+    return song_object_list
 
+def download_songs(song,thread_number):
+    
+
+    """
     for song in song_object_list : #[:3]
         url = yt.search_song(song)
         print(song)
         file_name = gen_file_name(song.name, song.artist)
+    """
+    file_name = gen_file_name(song.name, song.artist)
+    yt = YoutubeApi()
+    url = yt.search_song(song)
+    yt.download([url], file_name)
 
-        yt.download([url], file_name)
+    scl = SpotifyCoverLoader(song.album_url)
+    # needs to pass song.name to add to albumcover.jpg so the system does not have problems bc several files are calles albumcover.jpg
+    scl.download_cover(song.name)
+    scl.merge_cover(file_name,song.artist)
 
-        scl = SpotifyCoverLoader(song.album_url)
-        scl.download_cover()
-        scl.merge_cover(file_name,song.artist)
+def test(a):
+
+    print("test")
+
+def main() :
+    # Pulls info from spotify using the spotify api
+    song_object_list = pull_spotify_info()
+
+    # Searches Youtube to find the matching song, downloads it and adds the specific cover
+    mp = MultiprocessStart()
+    mp.start_multiple_instances(amount_of_threads=6 ,target_function=download_songs,song_object_list=song_object_list)
+    #mp.start_multiple_instances(amount_of_threads=6,)
+    #download_songs(song_object_list)
+
+
 
 
 if __name__ == '__main__' :
