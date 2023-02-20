4f33183975d5eb5af76d9dbf75861ffec38c1225
HyperCaption <alex.faehndrich@gmail.com>
2023-02-20 20:12:12 +0100

    bug fix

---
+++ modules/str_tools.py
@@ -0,0 +1,21 @@
+
+import urllib
+
+
+def clean_name(name) :
+    new_name = name\
+        .replace(" ", "+")\
+        .replace("ë", "e")\
+        .replace("ä", "ae")\
+        .replace("ü", "ue")\
+        .replace("ö", "oe")\
+        .replace(":", "")\
+        .replace("<", "")\
+        .replace(">", "")\
+        .replace("/", "")
+
+                
+        
+    new_name = urllib.parse.quote_plus(new_name)
+
+    return new_name
