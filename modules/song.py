85cbeeff13d76c62fa20cd2791d0ed6138be49cd
Muezzin-ay <hartrobin05@gmail.com>
2023-03-03 20:31:29 +0100

    improved spotify api connection

--- modules/song.py
+++ modules/song.py
@@ -3,11 +3,11 @@ from modules.name_tools import NameTools
 
 
 class Song():
-    def __init__(self, artist, name, duration, cover_url) :
-        self.name = name
-        self.artist = artist
-        self.duration = duration
-        self.cover_url = cover_url
+    def __init__(self, song_data) :
+        self.name = song_data['name']
+        self.artist = song_data['artists'][0]['name']
+        self.duration = int(song_data['duration_ms'] / 1000)
+        self.cover_url = song_data['album']['images'][0]['url']
         self.file_name = NameTools.gen_file_name(self)
 
     def get_search_word(self) :
--- modules/spotify.py
+++ modules/spotify.py
@@ -1,7 +1,6 @@
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
-from pandas import DataFrame, concat
 
 from modules.song import Song
 
@@ -18,45 +17,13 @@ class SpotifyApi(Spotify):
                                 "time_signature"]
 
     def get_songs_from_playlist(self, playlist_id, song_queue):
-        playlist_df = DataFrame(columns=self.playlist_attributes_list)
         playlist = self.user_playlist_tracks(self.sp_user_name, playlist_id)["items"]
-        for track in playlist:
-            playlist_features = {}
-            playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
-            playlist_features["album"] = track["track"]["album"]["name"]
-            playlist_features["track_name"] = track["track"]["name"]
-            playlist_features["track_id"] = track["track"]["id"]
-
-            audio_features = self.audio_features(playlist_features["track_id"])[0]
-            for feature in self.playlist_attributes_list[4:]:
-                playlist_features[feature] = audio_features[feature]
-
-            track_df = DataFrame(playlist_features, index=[0])
-            playlist_df = concat([playlist_df, track_df], ignore_index=True)
-            self.playlist_df = playlist_df
-
-        self.format_output(song_queue)
-
-    def format_output(self, song_queue):
-        for counter in range(len(self.playlist_df)):
-            artist_name = self.playlist_df["artist"][counter]
-            song_name = self.playlist_df["track_name"][counter]
-            song_duration = int(self.playlist_df["duration_ms"][counter]/1000)
-
-            track_id = self.playlist_df["track_id"][counter]
-            track_data = self.track(track_id)
-            cover_url = track_data['album']['images'][0]['url']
-
-            song = Song(artist_name, song_name, song_duration, cover_url)
+        for song_data in playlist :
+            song = Song(song_data['tracks'])
             song_queue.put(song)
     
 
     def search_for_song(self, search_words, song_queue) :
-        search_res = self.search(search_words, type='track', limit=1)['tracks']['items'][0]
-        song_name = search_res['name']
-        cover_url = search_res['album']['images'][0]['url']
-        artist_name = search_res['album']['artists'][0]['name']
-        song_duration = int(search_res['duration_ms'] / 1000)
-
-        song = Song(artist_name, song_name, song_duration, cover_url)
+        song_data = self.search(search_words, type='track', limit=1)['tracks']['items'][0]
+        song = Song(song_data)
         song_queue.put(song)
