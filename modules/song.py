90e9bc8c714a79264b1a07f8cd123b8fc0140eda
Muezzin-ay <hartrobin05@gmail.com>
2023-03-03 21:16:19 +0100

    multiple artists are now included

--- modules/song.py
+++ modules/song.py
@@ -5,10 +5,19 @@ from modules.name_tools import NameTools
 class Song():
     def __init__(self, song_data) :
         self.name = song_data['name']
-        self.artist = song_data['artists'][0]['name']
         self.duration = int(song_data['duration_ms'] / 1000)
         self.cover_url = song_data['album']['images'][0]['url']
+        self.gen_artist_listing(song_data['artists'])
+
         self.file_name = NameTools.gen_file_name(self)
+
+    def __repr__(self) -> str:
+        return f"{self.name} from {self.artist} ({self.duration}s)"
+    
+    def gen_artist_listing(self, artist_data) :
+        self.artist = artist_data[0]['name']
+        for artist in artist_data[1:] :
+            self.artist += f", {artist['name']}"
 
     def get_search_word(self) :
         searchable_name = NameTools.clean_name(self.name)
@@ -18,9 +27,6 @@ class Song():
     
     def get_file_name(self) :
         return self.file_name
-    
-    def __repr__(self) -> str:
-        return f"{self.name} from {self.artist} ({self.duration}s)"
     
     def check_duration(self, yt_duration) :
         #offset = abs(self.duration / yt_duration) #0.95 -> with Percentage
