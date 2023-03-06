fdc09af1fa64b7ec4f87ea7cc9647149e0f8c2a5
Robin Hart <hartrobin05@gmail.com>
2023-03-06 07:55:18 +0100

    lol

--- modules/yt_api.py
+++ modules/yt_api.py
@@ -49,7 +49,7 @@ class YoutubeApi :
                 'preferredquality': '320',
             }],
             'proxy' : '',
-            'user-agent' : 'Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0'
+            'user-agent' : ''
         }
         dlp = yt_dlp.YoutubeDL(ydl_ops)
         dlp.download(urls)
